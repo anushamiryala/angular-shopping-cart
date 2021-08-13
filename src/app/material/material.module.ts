83ce5cc3475d65b70e45f7683536e67045303f75
anushamiryala <49860105+anushamiryala@users.noreply.github.com>
2021-08-13 11:34:56 +0530

    Update material.module.ts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,4 +1,4 @@
-// import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
 // import {MatBadgeModule} from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
